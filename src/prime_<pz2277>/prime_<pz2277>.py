917f5d365c0ea8f091caf6923a50a1af8b0be7a9
Peizhi Zhang <pz2277@columbia.edu>
2023-11-01 18:04:55 -0400

    Add docstrings to is_prime function

--- src/prime_<pz2277>/prime_<pz2277>.py
+++ src/prime_<pz2277>/prime_<pz2277>.py
@@ -1,20 +1,24 @@
 import math
 
 def is_prime(n):
-    """Check if a number is a prime number.
 
-    Args:
-        n (int): The number to check.
-
-    Returns:
-        bool: True if n is a prime number, False otherwise.
-
-    Example:
-        >>> is_prime(5)
-        True
-        >>> is_prime(4)
-        False
     """
+    Determine whether a number is a prime number.
+    
+    :param n: The number to be checked.
+    :type n: int
+    :return: True if n is a prime number, False otherwise.
+    :rtype: bool
+    
+    Example:
+    >>> is_prime(5)
+    True
+    
+    >>> is_prime(4)
+    False
+    
+    """
+    
     if n <= 1:
         return False
     for i in range(2, int(math.sqrt(n)) + 1):
